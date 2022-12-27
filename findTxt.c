b9c357946049068a16cb5f49042ade3316e5144e
Moshe Ofer <107894139+MosheOfer1@users.noreply.github.com>
2022-12-27 10:44:29 +0200

    Update findTxt.c

--- findTxt.c
+++ findTxt.c
@@ -7,9 +7,9 @@
 // getline: read a line into s, return length
 int getline1(char s[])
 {
-    int c, i;
+    int c = 0;
 
-    for (i = 0; i < LINE - 1 && c != '\n' && c!='\r'; ++i){
+    for (int i = 0; i < LINE - 1 && c != '\n' && c!='\r'; ++i){
         if((c = getchar()) != EOF)
             if(c != '\r')
                 s[i] = c;
